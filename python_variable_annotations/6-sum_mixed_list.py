7acc5179f034185aac21e7dad1d171dc306bfbc5
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-29 10:25:49 +1000

    Union

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contain a function sum_list
+"""
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    """return sum of list"""
+    return sum(mxd_lst)
