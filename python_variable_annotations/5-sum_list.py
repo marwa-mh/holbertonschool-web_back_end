097cf4890c6729ef1fbe4c0b2296ef57db617787
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-29 10:21:06 +1000

    typing List

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contain a function sum_list
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """return sum of list"""
+    return sum(input_list)
