6b919339d1db488fb1bd4d9cee9b2421cd2377ad
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-29 10:37:26 +1000

    Tuple

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,6 @@ This module contain a function sum_list
 from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst:List[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return sum of list"""
     return sum(mxd_lst)
---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contain a function sum_list
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return sum of list"""
+    result =  (k, v**2)
+    return result
