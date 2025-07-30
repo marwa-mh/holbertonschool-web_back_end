e993d43a9ca2bfb5a364af0e2703dc49ea110669
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-30 15:32:22 +1000

    last task

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module """
+import typing
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Generator that sleeps 10 times then returns random float"""
+    start = time.perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension())
+    end = time.perf_counter()
+    
+    return end - start
