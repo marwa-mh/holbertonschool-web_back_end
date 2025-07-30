7a8cb84fc4d901a3f5ab1be6cf536c954fc3c77e
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-30 15:21:31 +1000

    1-async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> typing.List[float]:
     """Generator that sleeps 10 times then returns random float"""
 
-    result = [value async for value in async_comprehension()]
+    result = [value async for value in async_generator()]
     return result
