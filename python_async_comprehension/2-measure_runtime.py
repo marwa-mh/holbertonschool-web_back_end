a99f09d9232a09cb7e42f64e1345a159be1f436b
Marwa-m <marwa.almahmoud@hotmail.com>
2025-07-30 15:36:27 +1000

    It was working but the checker is shit

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Generator that sleeps 10 times then returns random float"""
     start = time.perf_counter()
-
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*tasks)
     end = time.perf_counter()
     
     return end - start
