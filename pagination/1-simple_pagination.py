c5e456ccedec6ff039ef19723102b5e6a3b3718d
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-11 11:23:04 +1000

    1-simple_pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,36 @@
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int):
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        if not isinstance(page, int) or not isinstance(page_size, int):
+            raise AssertionError("The value should be integer")
+        assert page > 0
+        assert page_size > 0
+        data = self.dataset()
+        (start, end) = index_range(page, page_size)
+        return data[start:end]
