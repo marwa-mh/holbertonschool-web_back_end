7e9ca5bd07c90208f8149f105c9d04ce5d07d835
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-14 10:29:54 +1000

    10-update_topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" 10-update_topics """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Insert new document"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
