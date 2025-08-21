34e5edcb0496ecc2fe441832162881b1651ee60b
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-22 07:28:00 +1000

    5-building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,28 @@
+export default class Building {
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error("Cannot instantiate abstract class Building")
+        }
+        this.validateAndAssignSqft(sqft);
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(sqft) {
+        this.validateAndAssignSqft(sqft);
+    }
+
+    validateAndAssignSqft(sqft) {
+      if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new TypeError('sqft must be a number');
+    }
+    }
+
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    } 
+}
