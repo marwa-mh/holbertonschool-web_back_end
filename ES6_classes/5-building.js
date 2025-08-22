02f353e7c46131e994f0c6efa78ddbd011fdbe3b
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-22 10:54:28 +1000

    5-building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,20 +1,29 @@
 export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-
-    // Ensure that any subclass implements evacuationWarningMessage
-    if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === undefined
-    ) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+    constructor(sqft) {
+        
+        this.validateAndAssignSqft(sqft);
+        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
     }
-  }
 
-  // Getter for sqft
-  get sqft() {
-    return this._sqft;
-  }
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(sqft) {
+        this.validateAndAssignSqft(sqft);
+    }
+
+    validateAndAssignSqft(sqft) {
+      if (typeof sqft === 'number') {
+      this._sqft = sqft;
+    } else {
+      throw new TypeError('sqft must be a number');
+    }
+    }
+
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    } 
 }
