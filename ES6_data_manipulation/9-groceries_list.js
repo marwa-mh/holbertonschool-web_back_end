f4d62f9395f928ae4fbe6924e5ccdacb9b167b58
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-23 07:55:23 +1000

    9-Map

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+    const groceries_list = new Map([
+        ["Apples", 10],
+        ["Tomatoes", 10],
+        ["Pasta", 1],
+        ["Rice", 1],
+        ["Banana", 5]
+            
+    ]);
+
+    return groceries_list;
+}
