72cb22c77aee41ec1544ec16530e2be5cd5fc4d5
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-25 14:00:07 +1000

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-   return promise.then({
+   return promise.then(()=> {
+    return {
     
         status: 200,
         body: 'success'
     }
+}
    )
    .catch(()=> {
     return new Error();
