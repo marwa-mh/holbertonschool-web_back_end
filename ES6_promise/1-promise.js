f99c3b5ca1b5a5f6e899186a204dd3649c1aa550
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-25 13:04:03 +1000

    1-promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+    if (success) {
+        return Promise.resolve({
+            status: 200,
+            body: 'Success',
+        });
+    } else
+    {
+        return Promise.reject("The fake API is not working currently");
+    }
+}
