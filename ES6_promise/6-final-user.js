2778ab371d2030ef571403dcee51518963edd851
Marwa-m <marwa.almahmoud@hotmail.com>
2025-08-25 16:12:04 +1000

    final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,12 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise.allSettled(
-        [signUpUser(firstName, lastName), uploadPhoto(fileName)]
+        [signUpUser(firstName, lastName),
+             uploadPhoto(fileName),]
     ).then((result) => result.map((res) => {
-        if (res.status === 'fulfilled') {
-            return {status: res.status, value: res.value};
-        }
-        return {status: res.status, value: res.reason};
+        
+        return {status: res.status, value: res.value};
     }));
    
     
